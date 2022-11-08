e126913177d09d2a931a12b0cee49ac6df70884b
ghalifaten <faten.ghali@epfl.ch>
2022-11-08 15:34:54 +0100

    add night slider, labels and restyle x-axis ticks

--- web_interface/src/static/js/barChart.js
+++ web_interface/src/static/js/barChart.js
@@ -1,21 +1,17 @@
 // set the dimensions and margins of the graph
 var margin = {top: 0, right: 30, bottom: 90, left: 60},
-    width = 460 - margin.left - margin.right,
+    width = 800 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("#bar_chart")
             .append("svg")
             .attr("class", "svg-style")
-            //.attr("width", width + margin.left + margin.right)
-            //.attr("height", height + margin.top + margin.bottom)
             .append("g");
-            //.attr("transform",
-              //    "translate(" + margin.left + "," + margin.top + ")")
 
 
 // initialize variables of sliders
-var morningValue = 0, middayValue = 0, afternoonValue = 0, eveningValue = 0;
+var morningValue = 0, middayValue = 0, afternoonValue = 0, eveningValue = 0, nightValue = 0;
 
 // Parse the Data
 d3.csv("static/data/XYZ.csv", function(data) {
@@ -24,13 +20,16 @@ d3.csv("static/data/XYZ.csv", function(data) {
     var x = d3.scaleBand()
                 .range([ 0, width ])
                 .domain(data.map(function(d) { return d.Period; }))
-                .padding(0.7);
+                //.padding(0.7);
+                .paddingInner(.1)
+                .paddingOuter(.3)
 
     svg.append("g")
         .attr("transform", "translate(0," + height + ")")
         .call(d3.axisBottom(x))
-        .selectAll("text")
-        .attr("transform", "translate(-10,0)rotate(-45)")
+        .selectAll(".tick text")
+        .call(wrap, x.bandwidth())
+        .attr("transform", "translate(50,0)")
         .style("text-anchor", "end");
 
     // Add Y axis
@@ -48,20 +47,22 @@ d3.csv("static/data/XYZ.csv", function(data) {
         .append("rect")
         .attr("x", function(d) { return x(d.Period); })
         .attr("y", function(d) { return y(d.Value); })
-        .attr("width", x.bandwidth())
+        .attr("width", 20)
         .attr("height", function(d) { return height - y(d.Value); })
         .attr("fill", "#69b3a2")
         .attr("rx", 10)
         .attr("ry", 10)
+        .attr("transform", "translate(48,0)")
         .attr("class", "baseline-bar")
 
     // A function that update the chart when slider is moved?
-        function updateChart(morningValue, middayValue, afternoonValue, eveningValue) {
+        function updateChart(morningValue, middayValue, afternoonValue, eveningValue, nightValue) {
             //TODO optimize this
             data[0].Value = morningValue;
             data[1].Value = middayValue;
             data[2].Value = afternoonValue;
             data[3].Value = eveningValue;
+            data[4].Value = nightValue;
 
             svg.selectAll(".baseline-bar")
                 .attr("fill", "#D3D3D3");
@@ -74,11 +75,12 @@ d3.csv("static/data/XYZ.csv", function(data) {
                 .append("rect")
                 .attr("x", function(d) { return x(d.Period); })
                 .attr("y", function(d) { return y(d.Value); })
-                .attr("width", x.bandwidth())
+                .attr("width", 20)
                 .attr("height", function(d) { return height - y(d.Value); })
                 .attr("fill", "#69b3a2")
                 .attr("rx", 10)
                 .attr("ry", 10)
+                .attr("transform", "translate(60,0)")
                 .attr("class", "new-bar")
         }
 
@@ -99,9 +101,36 @@ d3.csv("static/data/XYZ.csv", function(data) {
     d3.select("#eveningSlider").on("change", function(d){
         eveningValue = this.value
         })
+    //night
+    d3.select("#nightSlider").on("change", function(d){
+        nightValue = this.value
+        })
 
     // Listen to the submit button
     d3.select("#submit-btn").on("click", function(d){
-        updateChart(morningValue, middayValue, afternoonValue, eveningValue)
+        updateChart(morningValue, middayValue, afternoonValue, eveningValue, nightValue)
         })
 });
+
+function wrap(text, width) {
+    text.each(function() {
+      var text = d3.select(this),
+          words = text.text().split(/\s+/).reverse(),
+          word,
+          line = [],
+          lineNumber = 0,
+          lineHeight = 1.1, // ems
+          y = text.attr("y"),
+          dy = parseFloat(text.attr("dy")),
+          tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em")
+      while (word = words.pop()) {
+        line.push(word)
+        tspan.text(line.join(" "))
+        if (tspan.node().getComputedTextLength() > width) {
+          line.pop()
+          tspan.text(line.join(" "))
+          line = [word]
+          tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", `${++lineNumber * lineHeight + dy}em`).text(word)
+        }
+      }
+    })}
