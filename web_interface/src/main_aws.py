dc02bf85b8f26aacd85f0e94717857c9f68364ea
ghalifaten <faten.ghali@epfl.ch>
2023-02-02 13:07:06 +0100

    complete structure and clean-up code

--- web_interface/src/main_aws.py
+++ web_interface/src/main_aws.py
@@ -1,4 +1,4 @@
-#http://127.0.0.1:5000//data?m=1&ID=QR001&hh_size=1&hh_type=1&frequency=2&weekly_freq=3
+#http://127.0.0.1:5000//data?m=1&ID=QR001&hh_size=1&hh_type=1&frequency=2
 from flask import Flask, request, render_template, jsonify, session
 import os, sys, json
 import datetime
@@ -7,13 +7,12 @@ import boto3
 import conf.credentials as conf
 import secrets
 
-secret = secrets.token_urlsafe(32) #generate secret key for the current session
-
+#---- SET UP PATH ----#
 module_path = os.path.abspath(os.path.join('..'))+'/MoMeEnT-Project'
 if module_path not in sys.path:
     sys.path.append(module_path)
 
-###########
+#---- SET UP CONNECTIONS ----#
 client = boto3.client('lambda',
                         region_name= conf.region,
                         aws_access_key_id=conf.aws_access_key_id,
@@ -26,10 +25,11 @@ dynamodb = boto3.resource('dynamodb',
 
 table = dynamodb.Table('MockMomeentProjectData')
 
+secret = secrets.token_urlsafe(32) #generate secret key for the current session
 app = Flask(__name__, template_folder='templates')
 app.secret_key = secret
 
-############
+#---- INITIALIZE VARIABLES ----#
 n_households = 1000
 usage_patterns = {'target_cycles':{'DISH_WASHER':(np.ones(n_households)*251).tolist(),#/!\.tolist() is necessary to make ndarrays JSON serializable
                                     'WASHING_MACHINE':(np.ones(n_households)*100).tolist()},
@@ -38,7 +38,7 @@ usage_patterns = {'target_cycles':{'DISH_WASHER':(np.ones(n_households)*251).tol
                                        }
                 }
 
-############
+#---- FLASK ROUTES ----#
 @app.route('/<qualtrics_data>')
 def index(qualtrics_data):
     try:
@@ -67,52 +67,148 @@ def index(qualtrics_data):
     session["hh_size"] = hh_size
     session["hh_type"] = hh_type
 
+    #save responseID for further DB updates
+    sassion["ID"] = ID
+
     #create an item (DB record)
     item = {
         "m": m,
         "ResponseID": ID,
         "hh_size": hh_size,
         "hh_type": hh_type,
-        "frequency": frequency
+        "frequency": weekly_freq
     }
     #add item to DB
     table.put_item(Item=item)
 
-    #also send item to the web_interface as a JSON object for localStorage
-    qualtrics_data = json.dumps(item)
+    return render_template("index.html")
 
-    return render_template("index.html", qualtrics_data=qualtrics_data)
 
-@app.route('/experiment0')
-def experiment0():
+
+@app.route('/experiment_0')
+def experiment_0():
     return render_template("experiment_0.html")
 
-@app.route('/questions', methods=['GET','POST'])
-def questions():
-    return render_template("questions.html")
+@app.route('/questions_0', methods=['GET','POST'])
+def questions_0():
+    return render_template("questions_0.html")
 
-@app.route('/experiment1')
+
+
+@app.route('/experiment_1')
 def experiment_1():
+    #retrieve answers to questions_0 here
+    q0_answers = request.args
+    #/!\ args for now look like this: 
+    #ImmutableMultiDict([('q0_r1', '1'), ('q0_r2', 'hello'), ('q0_r3', 'washmachine'), ('q0_r3', 'dryer'), ('q0_r3', 'fridge')])
+    #TODO manipulate answers here to get the desired type
+    #save answers to DB
+    #table.set_item() #need the responseID to set the corresponding record
+
     return render_template("experiment_1.html")
 
+@app.route('/questions_1a', methods=['GET','POST'])
+def questions_1a():
+    return render_template("questions_1a.html")
+
+@app.route('/questions_1b', methods=['GET','POST'])
+def questions_1b():
+    q1a_answers = request.args
+    #TODO manipulate answers here to get the desired type
+    #save answers to DB with the corresponding ID from the session
+    #ResponseID = session["ID"]
+    #table.set_item(ResponseID ...) 
+
+    return render_template("questions_1b.html")
+
+
+
+@app.route('/experiment_2')
+def experiment_2():
+    q1b_answers = request.args
+    #TODO manipulate answers here to get the desired type
+    #save answers to DB
+    #table.set_item() #need the responseID to set the corresponding record
+    
+    return render_template("experiment_2.html")
+
+@app.route('/questions_2a', methods=['GET','POST'])
+def questions_2a():
+    return render_template("questions_2a.html")
+
+@app.route('/questions_2b', methods=['GET','POST'])
+def questions_2b():
+    q2a_answers = request.args
+    #TODO manipulate answers here to get the desired type
+    #save answers to DB
+    #table.set_item() #need the responseID to set the corresponding record
+
+    return render_template("questions_2b.html")
+
+
+
+@app.route('/experiment_3')
+def experiment_3():
+    q2b_answers = request.args
+    #TODO manipulate answers here to get the desired type
+    #save answers to DB
+    #table.set_item() #need the responseID to set the corresponding record
+    
+    return render_template("experiment_3.html")
+
+@app.route('/questions_3a', methods=['GET','POST'])
+def questions_3a():
+    return render_template("questions_3a.html")
+
+@app.route('/questions_3b', methods=['GET','POST'])
+def questions_3b():
+    q3a_answers = request.args
+    #TODO manipulate answers here to get the desired type
+    #save answers to DB
+    #table.set_item() #need the responseID to set the corresponding record
+    
+    return render_template("questions_3b.html")
+
+
+
+@app.route('/experiment_4')
+def experiment_4():    
+    q3b_answers = request.args
+    #TODO manipulate answers here to get the desired type
+    #save answers to DB
+    #table.set_item() #need the responseID to set the corresponding record
+    
+    return render_template("experiment_4.html")
+
+@app.route('/questions_4a', methods=['GET','POST'])
+def questions_4a():
+    return render_template("questions_4a.html")
+
+@app.route('/questions_4b', methods=['GET','POST'])
+def questions_4b():    
+    q4a_answers = request.args
+    #TODO manipulate answers here to get the desired type
+    #save answers to DB
+    #table.set_item() #need the responseID to set the corresponding record
+    
+    return render_template("questions_4b.html")
+
+
+
+@app.route('/conclusion')
+def conclusion():
+    q4b_answers = request.args
+    #TODO manipulate answers here to get the desired type
+    #save answers to DB
+    #table.set_item() #need the responseID to set the corresponding record
+    
+    return render_template("conclusion.html")
+
+
+#---- COST FUNCTION ----# (TO BE CALLED LOAD FUNCITON LATER)
 @app.route('/get-cost', methods=['POST'])
 def get_cost():
-    """
-        We used to go through localStorage on the browser to move variables around different routes (using ajax)
-    """
-    """
-    #variables coming from ajax request (see barchart_1.js)
-    n_residents = request.get_json()['n_residents']
-    household_type = request.get_json()['household_type']
-    try:
-        n_residents = int(n_residents)
-        household_type = int(household_type)
-    except:
-        return 'error'
-    """
-    """
-        Now we use Flask session
-    """
+    #retrieve hh_size and hh_type from session
     n_residents = session["hh_size"]
     household_type = session["hh_type"]
 
@@ -143,30 +239,6 @@ def get_cost():
     return jsonify(api_response)
 
 
-@app.route('/experiment2')
-def experiment_2():
-    #demo_qualtrics_function()
-    return render_template("experiment_2.html")
-
-@app.route('/experiment2', methods=['POST'])
-def experiment_2_post():
-    nresidents = request.form['nresidents']
-    nhouseholds = request.form['nhouseholds']
-    #demo_qualtrics_function(n_households=int(nhouseholds))
-    return render_template("experiment_2.html", nresidents=nresidents, nhouseholds=nhouseholds)
-
-@app.route('/experiment3')
-def experiment_3():
-    return render_template("experiment_3.html")
-
-@app.route('/experiment4')
-def experiment_4():
-    return render_template("experiment_4.html")
-
-
-@app.route('/conclusion')
-def conclusion():
-    return render_template("conclusion.html")
-
+#---- MAIN CALL ----# 
 if __name__ == "__main__":
     app.run(debug=True)
