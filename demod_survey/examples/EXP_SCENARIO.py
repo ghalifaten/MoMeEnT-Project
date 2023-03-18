22f102c213f85f57f97c608a76308f83bab8510f
BarsantiMatteo <71601250+BarsantiMatteo@users.noreply.github.com>
2023-03-18 19:50:26 +0100

    updated load simulator - quicker and more robust

---
+++ demod_survey/examples/EXP_SCENARIO.py
@@ -0,0 +1,178 @@
+"""
+DEMO FILE FOR QUALTRICS API INTEGRATION
+"""
+
+import os
+import sys
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
+import datetime
+from datetime import timedelta
+import time
+
+module_path = os.path.abspath(os.path.join('..'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+
+from demod.datasets.Germany.loader import GermanDataHerus
+
+from EXP_SIMULATOR import load_simulator
+
+
+#%% INPUTS
+
+"""
+For the time being, we can simplify the problem and consider only the type of 
+household and the number of residents as input. The input dictionary is as follows:
+    - 'n_residents' : int from 1 to 5
+    - 'household_type' : int from 1 to 5
+        1 = One person household
+        2 = Couple without kid
+        3 = Single Parent with at least one kid under 18 and the other under 27
+        4 = Couple with at least one kid under 18 and the other under 27
+        5 = Others
+        
+        subgroups = [{'n_residents': 1, 'household_type': 1, 'weekday':[1,2,3,4,5]},
+                      {'n_residents': 2, 'household_type': 2, 'weekday':[1,2,3,4,5]},
+                      {'n_residents': 2, 'household_type': 3, 'weekday':[1,2,3,4,5]},
+                      {'n_residents': 3, 'household_type': 3, 'weekday':[1,2,3,4,5]},
+                      {'n_residents': 3, 'household_type': 4, 'weekday':[1,2,3,4,5]},
+                      {'n_residents': 4, 'household_type': 4, 'weekday':[1,2,3,4,5]},
+                      {'n_residents': 5, 'household_type': 4, 'weekday':[1,2,3,4,5]}]
+        hh_subgroups = [subgroups[0]]
+        n_hh_list = [n_households]
+        type of hh: 1 single, 2 couple without children, 3 single parent, 4 couple with childrens
+"""
+
+
+# From qualtrics
+subgroups = [{'n_residents': 2, 'household_type': 2, 'weekday':[1,2,3,4,5]}]
+n_households = 10000
+
+# Other inputs
+# From qualtrics
+wm_cycles = 3
+dw_cycles = 4
+dict_progr = {}
+dict_progr['DISH_WASHER'] = {'ECO':1,
+                             'Normal':3,
+                             'Intensive':2,
+                             'Auto':2,
+                             'Gentle':0,
+                             'Quick low':1,
+                             'Quick high':0,
+                             }
+
+dict_progr['WASHING_MACHINE'] = {'30°C':1,
+                                 '40-45°C':3,
+                                 '55-60°C':2,
+                                 '90°C':6,
+                                }
+# From adaptive-experiment
+values_dict = {'morning':0,
+               'midday':1,
+               'afternoon':1,
+               'evening':1,
+               'night':1,
+               }
+
+# Other static inputs
+n_households = 10000
+
+
+#%% INPUTS PREPARATION
+
+# appliance usage pattern 
+usage_patterns = {'target_cycles':{},
+                  'day_prob_profiles':{},
+                  'energy_cycle':{},
+                  }
+
+
+# number of cycles 
+usage_patterns['target_cycles']['DISH_WASHER'] = int(dw_cycles * 52)
+usage_patterns['target_cycles']['WASHING_MACHINE'] = int(wm_cycles * 52)
+
+
+# usage profiles
+def movingaverage(interval, window_size):
+    window = np.ones(int(window_size))/float(window_size)
+    return np.convolve(interval, window, 'same')
+
+
+def generate_profile(values_dict):
+    raw_profile = np.asarray([values_dict['night']] * 6 * 6  +\
+                             [values_dict['morning']] * 4 * 6 + \
+                             [values_dict['midday']] * 4 * 6+ \
+                             [values_dict['afternoon']] * 4 * 6+ \
+                             [values_dict['evening']] * 4 * 6+ \
+                             [values_dict['night']] * 2 * 6                             
+                            )
+    # profile = movingaverage(raw_profile, 3)
+    return raw_profile
+
+usage_patterns['day_prob_profiles']['DISH_WASHER'] = generate_profile(values_dict)
+usage_patterns['day_prob_profiles']['WASHING_MACHINE'] = generate_profile(values_dict)
+
+
+# average energy intensity per cycle
+usage_patterns['energy_cycle']['DISH_WASHER'] = (dict_progr['DISH_WASHER']['ECO'] * 0.9 + \
+                                                dict_progr['DISH_WASHER']['Normal'] * 1.1 + \
+                                                dict_progr['DISH_WASHER']['Intensive'] * 1.44 + \
+                                                dict_progr['DISH_WASHER']['Auto'] * 0.93 + \
+                                                dict_progr['DISH_WASHER']['Gentle'] * 0.65 + \
+                                                dict_progr['DISH_WASHER']['Quick low'] * 0.8 + \
+                                                dict_progr['DISH_WASHER']['Quick high'] * 1.3 ) / \
+                                                sum(dict_progr['DISH_WASHER'].values())
+
+avg_wm_temp = (dict_progr['WASHING_MACHINE']['30°C'] * 30 + \
+               dict_progr['WASHING_MACHINE']['40-45°C'] * 42.5 + \
+               dict_progr['WASHING_MACHINE']['55-60°C'] * 57.5 + \
+               dict_progr['WASHING_MACHINE']['90°C'] * 90) / \
+               sum(dict_progr['WASHING_MACHINE'].values())
+ 
+usage_patterns['energy_cycle']['WASHING_MACHINE'] = 0.95 + 0.02 * (avg_wm_temp - 60)
+
+
+#%% SIMULATION
+
+start = time.time()
+
+# TODOs
+# 1. remove moving average
+# 2. update generate_profile (start from 0am and remove moving average)
+# 3. usage patterns --> day prob profile single array 
+# 4. usage patterns --> target cycle int 
+
+appliance = 'WASHING_MACHINE'
+
+load = load_simulator(
+        hh_subgroups = subgroups,        
+        n_hh_list = [n_households],
+        start_datetime = datetime.datetime(2014, 4, 1, 0, 0, 0),
+        data = GermanDataHerus(version='v1.1'),
+        appliance=appliance,
+        usage_patterns = usage_patterns,
+        )
+
+
+end = time.time()
+print('The simulation took {:0.1f} s'.format(end-start))
+
+
+
+fig, ax = plt.subplots()
+ax.plot(range(len(load)), load)
+ax1 = ax.twinx()
+esp_profile = np.repeat(usage_patterns['day_prob_profiles'][appliance], 10)
+ax1.plot(range(len(esp_profile)), esp_profile)
+plt.show()
+print('total energy consumption {:0.2f} kWh/day'.format(sum(load)/60/1000))
+
+
+
+
+
---
+++ demod_survey/examples/EXP_SIMULATOR.py
@@ -0,0 +1,145 @@
+import os
+import sys
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import datetime
+    
+module_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
+if module_path not in sys.path:
+    sys.path.append(module_path)
+
+from demod.datasets.Germany.loader import GermanDataHerus
+from demod.simulators.base_simulators import SimLogger
+from demod.simulators.activity_simulators import SubgroupsIndividualsActivitySimulator, SemiMarkovSimulator
+
+
+
+def rejection_sampling(
+        num_samples: int, x_min: float, x_max: float, profile: np.ndarray
+    ):
+    """ Sample from given distribution within an interval x_min-x_max
+    """   
+    x = np.random.randint(x_min, x_max, num_samples)
+    u = np.random.uniform(np.zeros_like(x), max(profile[x_min:x_max]))
+    (idx,) = np.where(u < profile[x])
+    return x[idx], len(idx) / num_samples
+   
+
+def slice_profile(profile, interval):
+    """ Slicing function from array, which handles consecutive and 
+    non consecutive intervals    
+    """
+    if interval[0] < interval[1]:
+        return profile[interval[0]:interval[1]]
+    else:
+        return np.append(profile[0:interval[1]], profile[interval[0]:]) 
+    
+
+def weighted_profile(profile, weights, intervals, n_cycles = 10000):
+    """ Function to weight various intervals of a distribution differently
+    """
+    
+    avg_weights = np.asarray([])
+    avg_profile = np.asarray([])
+    
+    for interval in intervals:        
+        val_prof_interval = slice_profile(profile, interval)      
+        avg_profile = np.append(avg_profile, [np.mean(val_prof_interval)] * len(val_prof_interval))
+        
+        val_weight_interval = slice_profile(weights, interval)         
+        avg_weights = np.append(avg_weights, [np.mean(val_weight_interval)] * len(val_weight_interval))
+    
+    output = np.roll(profile,-intervals[0][0]) / avg_profile   * avg_weights / sum(avg_weights) * n_cycles
+    output = np.roll(output,intervals[0][0])
+    
+    return output
+
+
+
+def load_simulator(
+        hh_subgroups : list,        
+        n_hh_list : list = [1000],
+        start_datetime : datetime.datetime = datetime.datetime(2014, 4, 1, 0, 0, 0),
+        data = GermanDataHerus(version='v1.1'),
+        appliance : str = None,
+        usage_patterns : dict = None,
+        ) -> float:
+    """
+    Simulator of the average daily load profile for wet appliances
+    washing machine and dishwasher.
+    """
+    
+    # initialization activitity simulatore
+    sim_act = SubgroupsIndividualsActivitySimulator(
+        hh_subgroups,
+        n_hh_list,
+        subsimulator=SemiMarkovSimulator,
+        data=data,
+        start_datetime = start_datetime,
+        use_week_ends_days=True,
+        logger=SimLogger('get_activity_states', aggregated=False)
+    )
+    
+    # run the simulation for 1 day
+    for i in range(144):
+        # update activity every 10 min    
+        sim_act.step()
+    
+    # retrieve the simulated load profile 
+    # DIM0: min of the day, DIM1: households, DIM2: appliances
+    activities = sim_act.logger.get('get_activity_states')
+    
+    standard_dict = {'morning':[6*6,10*6],
+                      'midday':[10*6,14*6],
+                      'afternoon':[14*6,18*6],
+                      'evening':[18*6,22*6],
+                      'night':[22*6,6*6],
+                      }
+    
+    dict_activities = {'DISH_WASHER':'dishwashing',
+                       'WASHING_MACHINE':'laundry'}
+
+    # weight the activity profile based on usage patterns day prob profile
+    weighted_act = weighted_profile(
+                    profile=activities[dict_activities[appliance]].sum(axis=1),
+                    weights=usage_patterns['day_prob_profiles'][appliance],
+                    intervals=list(standard_dict.values()),
+                    )
+    
+    weighted_act = [round(x) for x in weighted_act]    
+    
+    # estimate the load of of the activity profile
+    len_cycle = 6
+    cycle = np.ones(len_cycle) * 1000
+    load_profile0 = np.append(cycle, np.zeros(144-len(cycle)))
+    load_profiles = load_profile0[:,np.newaxis]
+    
+    for i in range(1,144):        
+        load_profiles = np.append(load_profiles, 
+                                  np.roll(load_profile0,i)[:,np.newaxis], 
+                                  axis=1)
+    
+    unweighted_load =np.sum( load_profiles * weighted_act, axis=1)
+    
+    # weight the mean daily load based on number of cycles
+    if usage_patterns:
+        if 'target_cycles' in usage_patterns.keys():
+            if appliance:
+                P_standby = 3 # [W]
+                # Energy per cycle 
+                # E cycle =  E_tot_cycle [kWh]- P_standby [W] * lenghty cycle [10 min] * 10 [10min/min]/ 60 [min/h] / 1000 [W/kW]
+                E_cycle = usage_patterns['energy_cycle'][appliance] - P_standby * len_cycle * 10 / 60 / 1000 
+                # Weighting factor
+                # N° cycles [cycles/year] * E cycle [kWh/cycle] / n days year [days/year] * 6 [10min/h] / (E cycles simulated [no energy unit * 10min] ) 
+                weight = usage_patterns['target_cycles'][appliance] * E_cycle / 365.25 * 6 / unweighted_load.sum() * 1000 # [W10min/-]
+                weighted_load = unweighted_load * weight + P_standby 
+    
+    output = np.repeat(weighted_load,10)
+    
+    return output
+                     
+    
+   
