8a8f7d492758ff2765ad0c7e3d10959a4385d6f5
ghalifaten <faten.ghali@epfl.ch>
2023-05-08 17:58:18 +0200

    translate radio-pop up back to English

--- web_interface/src/static/js/questions.js
+++ web_interface/src/static/js/questions.js
@@ -1,6 +1,4 @@
 const inputs  = document.getElementsByTagName("input")
-console.log(inputs[0])
-console.log(inputs[1])
 for (let i = 0; i < inputs.length - 1; i++) {
     inputs[i].required = true;
 }
